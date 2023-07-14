489cff5a6c4d03450392a9e2e782752d811bb0e6
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-14 16:15:06 +0000

    Add postadmin and commentadmin classes to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ['created_on', 'approved']
+    search_fields = ['name', 'email', 'body']
